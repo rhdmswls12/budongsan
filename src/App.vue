2a3f62437d18f73872adc4b31013859e09a6b123
rhdmswls12 <rhgusdnr1293@naver.com>
2022-08-08 14:12:38 +0900

    직방 웹 구현 시작

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
+
+  <div class="menu">
+    <a v-for="i in menus" :key="i">{{i}}</a>
+    
+  </div>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    직방
+    <h4>{{products[0]}}</h4>
+    <p>{{price1}}만원</p>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>{{price2}}만원</p>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>{{price2}}만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
+  data(){
+    return{
+      price1:60,
+      price2:70,
+      menus:['Home','Shop','About'],
+      products:['역삼동원룸','천호동원룸','마포구원룸'],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -22,5 +44,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu{
+  background:darkslateblue;
+  padding:15px;
+  border-radius:5px;
+}
+.menu a{
+  color:white;
+  padding:10px;
 }
 </style>
